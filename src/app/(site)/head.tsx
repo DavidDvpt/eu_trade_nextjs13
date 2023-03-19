a9f7919226fe0c8ca5a3ba90efaea3c5d17c4218
david <david.mosca69@gmail.com>
2023-03-19 01:57:43 +0100

    add schema to prisma

---
+++ src/app/(site)/head.tsx
@@ -0,0 +1,10 @@
+export default function Head() {
+  return (
+    <>
+      <title>EU Trade</title>
+      <meta content='width=device-width, initial-scale=1' name='viewport' />
+      <meta name='description' content='trade resources with eu trade' />
+      <link rel='icon' href='/favicon.ico' />
+    </>
+  );
+}
