6ccfee28fe44ca6bcfe1f94e809fc8a766fb7f46
david <david.mosca69@gmail.com>
2023-05-28 01:46:07 +0200

    update command_update

--- prisma/command_update.ts
+++ prisma/command_update.ts
@@ -7,8 +7,14 @@
 // } from '@prisma/client';
 // import client from './prismadb';
 
-import { getDistinctValues } from '@/lib/arrayTools';
-import { Transaction, TransactionType } from '@prisma/client';
+import { TransactionsExtended } from '@/app/extendedAppTypes';
+import {
+  SellStatus,
+  SessionStatus,
+  Transaction,
+  TransactionType,
+} from '@prisma/client';
+import { getDistinctValues } from '../src/lib/arrayTools';
 import client from './prismadb';
 
 // const createSessionsTupples = async () => {
@@ -44,12 +50,12 @@ import client from './prismadb';
 const createTransactiosnRelations = async () => {
   const buyTransactions = client.transaction.findMany({
     where: { type: TransactionType.BUY },
+    include: { item: true },
     orderBy: [{ createdAt: 'asc' }],
   });
 
-  buyTransactions.then(async (buys: Transaction[]) => {
+  buyTransactions.then(async (buys: TransactionsExtended) => {
     const distinctItems = getDistinctValues(buys.map((m) => m.itemId));
-    const sellLeft = 0;
 
     const request = async (iid: string, oid: string, qty: number) => {
       await client.transactionRelation.create({
@@ -61,34 +67,88 @@ const createTransactiosnRelations = async () => {
       });
     };
 
+    const soldCurrentSell = async (
+      e: Transaction,
+      itemBuy: Transaction,
+      bLeft: number,
+      sessionId: string
+    ) => {
+      request(itemBuy.id, e.id, bLeft);
+
+      await client.session.update({
+        where: { id: sessionId as string },
+        data: {
+          status: SessionStatus.SOLDED,
+        },
+      });
+    };
+
+    // loop on distinct items
     for (let i = 0; i < distinctItems.length; i++) {
       const singleItemBuys = buys.filter((f) => f.itemId === distinctItems[i]);
       let selectedBuy = 0;
       let buyLeft = singleItemBuys[selectedBuy].quantity;
 
       const sells = await client.transaction.findMany({
-        where: { type: TransactionType.SELL, itemId: distinctItems[i] },
+        where: { sellStatus: SellStatus.ENDED, itemId: distinctItems[i] },
       });
 
+      // loop on sells by item
       for (let i = 0; i < sells.length; i++) {
-        const e = sells[i];
-        if (buyLeft >= e.quantity) {
-          request(singleItemBuys[selectedBuy].id, e.id, e.quantity);
-          buyLeft -= e.quantity;
+        const s = sells[i];
+
+        if (buyLeft >= s.quantity) {
+          console.log('buyLeft', buyLeft, 'qty', s.quantity);
+          // request(singleItemBuys[selectedBuy].id, s.id, s.quantity);
+          buyLeft -= s.quantity;
         } else {
-          let sellLeft = e.quantity - buyLeft;
-          request(singleItemBuys[selectedBuy].id, e.id, buyLeft);
+          // soldCurrentSell(
+          //   s,
+          //   singleItemBuys[selectedBuy],
+          //   buyLeft,
+          //   buys[selectedBuy].sessionId as string
+          // );
+          let sellLeft = s.quantity - buyLeft;
+          console.log(
+            'buyLeft',
+            buyLeft,
+            'sellLeft',
+            sellLeft,
+            'qty',
+            s.quantity
+          );
           selectedBuy++;
           buyLeft = singleItemBuys[selectedBuy].quantity;
 
           while (sellLeft > 0) {
             if (buyLeft >= sellLeft) {
-              request(singleItemBuys[selectedBuy].id, e.id, sellLeft);
+              // request(singleItemBuys[selectedBuy].id, s.id, sellLeft);
               buyLeft -= sellLeft;
               sellLeft = 0;
+              console.log(
+                'buyLeft',
+                buyLeft,
+                'sellLeft',
+                sellLeft,
+                'qty',
+                s.quantity
+              );
             } else {
-              request(singleItemBuys[selectedBuy].id, e.id, buyLeft);
-              sellLeft = sellLeft - buyLeft;
+              // soldCurrentSell(
+              //   s,
+              //   singleItemBuys[selectedBuy],
+              //   buyLeft,
+              //   buys[selectedBuy].sessionId as string
+              // );
+              sellLeft -= buyLeft;
+              console.log(
+                'buyLeft',
+                buyLeft,
+                'sellLeft',
+                sellLeft,
+                'qty',
+                s.quantity
+              );
               selectedBuy++;
               buyLeft = singleItemBuys[selectedBuy].quantity;
             }
